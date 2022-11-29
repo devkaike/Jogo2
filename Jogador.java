02148d02cc74de3b0aaa40df876074c403ea59e5
Kaike silva <107559196+devkaike@users.noreply.github.com>
2022-11-29 15:30:23 -0300

    Update Jogador.java

--- Jogador.java
+++ Jogador.java
@@ -1,39 +1,29 @@
+import java.util.ArrayList;
 import java.util.Random;
 
 public class Jogador {
 	private String nome; 
-	private int tentativa;
+	private ArrayList<Integer> tentativa = new ArrayList<>();
 
-	public Jogador(String n) {
+	
+	public Jogador(String n, ArrayList<Integer> tentativas) {
 		nome = n;
+		setTentativa(tentativa);
+		this.tentativa = tentativa;
 	}
 	public Jogador() {
 		
 	}
 	
-	public int getTentativa() {
+	
+
+	public ArrayList<Integer> getTentativa() {
 		return tentativa;
 	}
-	public void setTentativa(int tentativa) {
+	public void setTentativa(ArrayList<Integer> tentativa) {
 		this.tentativa = tentativa;
 	}
-
-	/*Random aleatorio = new Random();
-	public String criaNome() {
-		String n = "";
-        char l;
-        int num;
-        num = aleatorio.nextInt(26)+ 65;
-        l = (char)num;
-        n += l;
-        for(int i = 0; i < 19; i++) {
-            num = aleatorio.nextInt(26) + 97;
-            l = (char)num;
-            n += l;
-        }
-        nome = n;
-        return nome;
-	}*/
+	
 	public String getNome() {
 		return nome;
 	}
